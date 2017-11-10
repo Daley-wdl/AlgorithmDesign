c8a055ff9f152594d4b53e44e9c3112d13a1b0fa
Mr-Dylan <1975214061@qq.com>
2017-11-10 22:32:00 +0800

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -4,13 +4,12 @@ class Solution {
         int[] temp=new int[2];
         for(int i=0;i<nums.length-1;i++){
             
-            temp[i]=nums[i];
             for(int j=i+1;j<nums.length;j++){
                 
                 if((nums[i]+nums[j])==target){
                     
-                    temp[0]=nums[i];
-                    temp[1]=nums[j];
+                    temp[0]=i;
+                    temp[1]=j;
                 }
                    
             }
